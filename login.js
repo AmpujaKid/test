6efc6ccc496867c7bdf2423af68b8bb9f7aea172
AmpujaKid <59574107+AmpujaKid@users.noreply.github.com>
2020-09-23 15:46:26 -0700

    now theres only one login screen

--- login.js
+++ login.js
@@ -1,11 +1,20 @@
 function pasuser(form) {
-if (form.id.value=="leetgrail") {
-if (form.pass.value=="krunker") {
-location="Home Page.html"
-} else {
-alert("Invalid Password")
-}
-} else {  alert("Invalid UserID")
-}
-}
+  if (form.id.value=="leetgrail") {
+      if (form.pass.value=="krunker") {
+        location="Home Page.html"
+      }
+    }
+
+    else if(form.id.value=="leetgraildev") {
+        if(form.pass.value=="krunker") {
+          location="devtools.html"
+        }
+        else {
+          alert("Invalid Password")
+        }
+      }
+      else {
+      alert("Invalid UserID")
+      }
+  }
 localStorage["id"] = form.id.value;
