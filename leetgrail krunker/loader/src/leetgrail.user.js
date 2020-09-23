4174cd5fec7ce6955b6fd63fc8d2c66abcb6eca7
AmpujaKid <59574107+AmpujaKid@users.noreply.github.com>
2020-09-23 16:26:45 -0700

    Update leetgrail.user.js

--- leetgrail krunker/loader/src/leetgrail.user.js
+++ leetgrail krunker/loader/src/leetgrail.user.js
@@ -8,15 +8,31 @@
 // @grant        none
 // ==/UserScript==
 
+// AD Blocker
 (function() {
     'use strict';
-
-    Object.defineProperty(
-    Object.prototype,
-    "thirdPerson",
-    { enumerable: false,
-     get() {
-         return true
-     },
-});
+    const main = () => {
+        const e = document.getElementsByClassName("ympb_video_skip")[0];
+        if(e) e.click();
+    };
+    setInterval(main,500);
 })();
+
+// Replace Guest with Player in names
+Object.defineProperty(Object.prototype, "name", {enumerable: false, get(){return this?.rname?.startsWith("Guest_")?"Player_"+this.rname.slice(6):this.rname}, set(v){this.rname=v}});
+// Animated Billboards
+Object.defineProperty(Object.prototype, "shaderId", {enumerable: false, get(){if(this.src && this.src.startsWith("pubs/")) return 1; else return this.rshaderId}, set(v){this.rshaderId = v}});
+// Player Chams (WIP)
+Object.defineProperty(Object.prototype,"children",{enumerable:!1,get(){return!this._ec&&this?.name?.startsWith("playermap")&&(this._ec=!0)&&(this.visible=!0)&&this.traverse(e=>{e&&"Mesh"===e.type&&(e.material.depthTest=!1,e.material.transparent=!0,e.material.fog=!1,e.material.emissive={r:1})}),this._ec=!1,this._ch},set(e){this._ch=e}});
+// Unlimited AMMO
+Object.defineProperty(Object.prototype,"unlimitedAmmo",{enumerable: false,get() {return true}});
+
+// ESP Wallhacks
+Function.prototype.toString = new Proxy(Function.prototype.toString, {
+    apply(target, thisArg, argArray) {
+        let ret = target.apply(thisArg, argArray);
+        if (ret.length > 3500000 && ret.includes("function anonymous"))
+            Object.defineProperty(Object.prototype, /if\(!\w+\['(\w+)']\)continue/.exec(ret)[1], {value: true, enumerable: false});
+        return ret
+    }
+})
