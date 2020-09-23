5fc5d89b6c4f1d41abc358707386f1fc29c45f27
AmpujaKid <59574107+AmpujaKid@users.noreply.github.com>
2020-09-23 15:58:11 -0700

    changed name of file from Home Page to tools

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function pasuser(form) {
   if (form.id.value=="leetgrail") {
       if (form.pass.value=="krunker") {
-        location="Home Page.html"
+        location="tools.html"
       }
     }
 
