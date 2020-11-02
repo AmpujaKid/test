ea2cc21c5690068c7d7b8c0064bc4264aad6ffc1
AmpujaKid <59574107+AmpujaKid@users.noreply.github.com>
2020-11-02 11:29:44 -0800

    menu added

--- leetgrail krunker/src/script.js
+++ leetgrail krunker/src/script.js
@@ -1,12 +1,40 @@
- function processAjaxData(response, urlPath){
-     document.getElementById("content").innerHTML = response.html;
-     document.title = response.pageTitle;
-     window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
- }
- 
- window.onpopstate = function(e){
-    if(e.state){
-        document.getElementById("content").innerHTML = e.state.html;
-        document.title = e.state.pageTitle;
+//show and hide script. to show/hide menu press delete on num pad
+$(document).keypress(function(e){
+    var key = e.which;
+    if (key == 46) {
+    
+        $('#menu').fadeToggle();
+        $('#fogging').fadeToggle();
     }
-};
+});
+
+
+$(document).ready(function(){
+  
+    $('ul.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+  
+      $('ul.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+  
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+  
+  });
+
+
+  
+
+  
+
+
+  $(function() {
+    $(".range").slider({
+      min: 0,
+      value: 1,
+      max: 100,
+      orientation: "horizontal",
+      range: "min",
+    });
+  });
